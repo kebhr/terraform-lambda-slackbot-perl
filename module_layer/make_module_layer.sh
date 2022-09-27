b3fe235e1561aa883eed7d0749c5436e98f85a5e
kebhr <42484226+kebhr@users.noreply.github.com>
2022-09-28 00:30:43 +0900

    chore: Module::Find を module_layer に含める

--- module_layer/make_module_layer.sh
+++ module_layer/make_module_layer.sh
@@ -6,5 +6,5 @@ docker run --rm \
     -v $(pwd):/var/task \
     -v $(pwd)/lib/perl5/site_perl:/opt/lib/perl5/site_perl \
     shogo82148/p5-aws-lambda:build-5.36.al2 \
-    cpanm --notest --no-man-pages WebService::Slack::WebApi
+    cpanm --notest --no-man-pages Module::Find WebService::Slack::WebApi
 zip -9 -r ../out/module-layer.zip .
